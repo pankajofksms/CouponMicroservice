0dfa38d8b52a54e5201c7db2ba99a14a99215012
pankaj <pankaj@192.168.29.172>
2021-02-23 10:18:04 +0530

    Add sleuth test logger

--- src/main/java/com/practice/couponservice/controller/CouponRestController.java
+++ src/main/java/com/practice/couponservice/controller/CouponRestController.java
@@ -1,5 +1,7 @@
 package com.practice.couponservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import com.practice.couponservice.repository.CouponRepo;
 @RestController
 @RequestMapping("/couponapi")
 public class CouponRestController {
+	
+	private static Logger logger=LoggerFactory.getLogger(CouponRestController.class);
 
 	@Autowired
 	CouponRepo repo;
@@ -28,6 +32,7 @@ public class CouponRestController {
 	@GetMapping(value = "/coupons/{code}")
 	public Coupon getCoupon(@PathVariable("code") String code) {
 		System.out.println("Server 2");
+		logger.info("Slueth Test in Coupon service");
 		return repo.findByCode(code);
 
 	}
